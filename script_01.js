43bce91a94ee662ea1327549c51c6fcc96c9c5b5
Danny <raducudaniel05@gmail.com>
2023-04-06 13:01:49 +0200

    update

--- script_01.js
+++ script_01.js
@@ -37,16 +37,53 @@
 // }
 
 // Inkrement (var. Schrittweite)
-// for (let i = 20; i <= 1666; i+=4) {
+// for (let i = 50; i <= 100; i+=5) {
 //     output("index i: " + i);
-//}
+// }
+
 
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (let i = 0; i < arr.length; i++) {
-   output("index i: " + i);  
-   
-}
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) {
+//     output("index i: " + i);
+//     output(arr[i]);
+// }
+
+ /*********   Überlegungen - Transponierung **********/
+
+ /* 
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    a = a + 1;
+//    output("inhalt von a: " + a);
+// }
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+// let str = ""; // Anfangswert
+// for (let i = 0; i < 4; i++) {
+//    str += "Test";
+//    output("inhalt von str: " + str);
+// }
 
 
 /**
@@ -55,44 +92,65 @@ for (let i = 0; i < arr.length; i++) {
  */
 
 
-
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-// function getSentence(word1,word2,word3,word4,word5,word6) {
-//     const GAP = " ";
-//     const PUNCT = ".";
-//     let str =   word1 + GAP +
-//                 word2 + GAP +
-//                 word3 + GAP +
-//                 word4 + GAP +
-//                 word5 + GAP +
-//                 word6 + 
-//                 PUNCT;
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP +
+                word5 + GAP +
+                word6 + 
+                PUNCT;
 
-//     return str;
-//}
+    return str;
+}
 
 /*** 01a. Funktionalität mit Array 1  */
 // Kritik | Überlegung
 // word1,word2, .... wordN :: Semantische Struktur
-// arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
+// arr[0],arr[1] ... arr[n] :: Numerische Struktur (0,1,2,3 ... n)
 // --> Transformation semantisches Problem --> numerisches Problem
 // output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-// function getSentenceArr(arr) {
-//     const GAP = " ";
-//     const PUNCT = ".";
-//     let str =   arr[0] + GAP +
-//                 arr[1] + GAP +
-//                 arr[2] + GAP +
-//                 arr[3] + GAP +
-//                 arr[4] + GAP +
-//                 arr[5] + 
-//                 PUNCT;
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] + 
+                PUNCT;
 
-//     return str;
-// }
+    return str;
+}
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = ""; 
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {
+            str += arr[i] + PUNCT; 
+        } else {
+            str += arr[i] + GAP;
+        }
+    }
+    
+     return str;
+
+}
+
 
 
 
